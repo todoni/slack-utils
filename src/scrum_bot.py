4092e171d38e0b05e06def8b5a23691fc49e7585
sohan <sohan@student.42seoul.kr>
2023-11-02 17:12:29 +0900

    Chore: scrum form 최상단 한번만 내용 나오고 밑으로는 이름만 나오게 수정

--- src/scrum_bot.py
+++ src/scrum_bot.py
@@ -9,10 +9,9 @@ def run_scrum_bot():
     channel_name = os.environ.get("SCRUM_CHANNEL_NAME")
     channel_id = slack.get_channel_id(channel_name, "private_channel")
     scrum_file = open("scrum.txt")
-    scrum_head = "<!channel> Today's Scrum has arrived! :love_letter:"
     scrum_body = scrum_file.read()
+    scrum_head = "<!channel> Today's Scrum has arrived! :love_letter:\n\n" + scrum_body + "\n"
     members_display_name = slack.get_channel_members(channel_id)
     slack.post_thread(channel_id, scrum_head)
     for name in members_display_name:
-        slack.post_thread(channel_id, ":sparkles: " +
-                          name + "\n\n" + scrum_body)
+        slack.post_thread(channel_id, ":sparkles: ", name)
