810afea6f97c343f4edf48e03ec81ea094e27039
JoonHyeok Han <54902347+Han-Joon-Hyeok@users.noreply.github.com>
2023-11-03 18:12:11 +0900

    Feat: Add printing members profile

--- src/slack_api.py
+++ src/slack_api.py
@@ -19,19 +19,20 @@ class slack_api:
     def get_channel_members(self, channel_id):
         result = self.client.conversations_members(channel=channel_id)
         members_id = result.data['members']
-        print(members_id)
         scrum_bot_id = os.environ.get("SCRUM_BOT_ID")
         github_bot_id = os.environ.get("APP_GITHUB_BOT_ID")
         members_id.remove(scrum_bot_id)
-        members_id.remove(github_bot_id)
+        #members_id.remove(github_bot_id)
         members_name = []
         for member_id in members_id:
             result = self.client.users_profile_get(user=member_id)
             profile = result.data['profile']
+            print(member_id, profile)
             name = profile['display_name']
             if name == '':
                 name = profile['real_name']
             members_name.append(name)
+        members_id.remove(github_bot_id)
         return members_name
 
     def post_thread(self, channel_id, text):
