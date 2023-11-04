0d2d0bb730be24dc5e23742d34ebae6aeebc706f
sohan <sohan@student.42seoul.kr>
2023-11-04 16:21:02 +0900

    Test: 작업 완료 될 때까지 post 하는 부분 주석처리

--- src/scrum_bot.py
+++ src/scrum_bot.py
@@ -3,16 +3,20 @@ import os
 import slack_api
 
 
-def run_scrum_bot():
-    token = os.environ.get("SLACK_BOT_USER_OAUTH_TOKEN")
-    slack = slack_api.slack_api(token)
-    channel_name = os.environ.get("SCRUM_CHANNEL_NAME")
-    channel_id = slack.get_channel_id(channel_name, "private_channel")
-    scrum_file = open("scrum.txt")
-    scrum_body = scrum_file.read()
-    scrum_head = "<!channel> Today's Scrum has arrived! :love_letter:\n\n" + scrum_body + "\n"
-    members_display_name = slack.get_channel_members(channel_id)
-    slack.post_thread(channel_id, scrum_head)
-    for name in members_display_name:
-        text = ":sparkles: " + name
-        slack.post_thread(channel_id, text)
+class scrum_bot:
+    def __init__(self, token):
+        self.token = token
+
+    def run_scrum_bot():
+        token = os.environ.get("SLACK_BOT_USER_OAUTH_TOKEN")
+        slack = slack_api.slack_api(token)
+        channel_name = os.environ.get("SCRUM_CHANNEL_NAME")
+        channel_id = slack.get_channel_id(channel_name, "private_channel")
+        scrum_file = open("scrum.txt")
+        scrum_body = scrum_file.read()
+        scrum_head = "<!channel> Today's Scrum has arrived! :love_letter:\n\n" + scrum_body + "\n"
+        members_display_name = slack.get_channel_members(channel_id)
+        # slack.post_thread(channel_id, scrum_head)
+        for name in members_display_name:
+            text = ":sparkles: " + name
+            # slack.post_thread(channel_id, text)
